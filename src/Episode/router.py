0cba2ff8607036361f9a90ff67df41f4021c6027
Sahil <sahiljahagirdar91@gmail.com>
2026-06-22 00:57:51 +0530

    Updated Rate limiting to endpoints

--- src/Episode/router.py
+++ src/Episode/router.py
@@ -11,31 +11,31 @@ from sqlalchemy.orm import session
 episode_route = APIRouter(prefix='/TMKOC',dependencies=[Depends(validate_api_key)])
 
 @episode_route.get('/episodes')
-@limiter.limit('10000/minute')
+@limiter.limit('100/minute')
 def all_episodes(request:Request,page: int = Query(1, ge=1),limit: int = Query(20, ge=1, le=100),db: session = Depends(get_db)):
     return controller.get_episodes(page, limit, db)
 
 @episode_route.get('/episode/{episode_id}',response_model=ResponseModel)
-@limiter.limit("10000/minute")
+@limiter.limit("100/minute")
 def one_episode(request:Request,episode_id:int,db:session = Depends(get_db)):
     return controller.get_one_episode(episode_id,db)
 
 @episode_route.get('/status')
-@limiter.limit("1000/minute")
+@limiter.limit("10/minute")
 def my_status(request:Request,db:session = Depends(get_db)):
     return controller.stats(db)
 
 @episode_route.get('/random',response_model=ResponseModel)
-@limiter.limit("3000/minute")
+@limiter.limit("30/minute")
 def get_random_episode(request:Request,db = Depends(get_db)):
     return controller.generate_random_episode(db)
 
 @episode_route.get('/latest_episode',response_model=ResponseModel)
-@limiter.limit("6000/minute")
+@limiter.limit("60/minute")
 def latest_episode(request:Request,db=Depends(get_db)):
     return controller.latest_episode(db)
 
 @episode_route.get('/search',response_model=PaginatedEpisodeResponse)
-@limiter.limit("2000/minute")
+@limiter.limit("20/minute")
 def search_episodes(request:Request,keyword: str,page: int = Query(1, ge=1),limit: int = Query(20, ge=1, le=100),db: session = Depends(get_db)):
     return controller.search_episodes(keyword, page, limit, db)
